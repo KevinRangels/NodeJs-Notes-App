6dd163e4b211fd3f9d9dd131e7c3417f6fb7855f
Kevin Rangel <krangel107@gmail.com>
2020-06-30 22:58:22 -0430

    define models

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+//Encriptar Password
+UserSchema.methods.encryptPassword = async password => {
+  const salt =  await bcrypt.genSalt(10)
+  return  await bcrypt.hash(password, salt)
+};
+
+//Cifrar Password and compare with the DB
+UserSchema.methods.matchPassword = function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+module.exports =  model('User', UserSchema)
