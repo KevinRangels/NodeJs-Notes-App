e167d4c7f872a32f9c3b8815e579e2dd77fd4ef6
Kevin Rangel <krangel107@gmail.com>
2020-07-08 08:23:19 -0430

    protected routes for users

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,20 +9,22 @@ const { renderNoteForm,
        deleteNote
      } = require('../controllers/notes.controller');
 
+const { isAuthenticated } = require('../herlpers/auth');
+
 //New note
-router.get('/notes/add', renderNoteForm);
-router.post('/notes/new-note', createNewNote);
+router.get('/notes/add', isAuthenticated, renderNoteForm);
+router.post('/notes/new-note', isAuthenticated, createNewNote);
 
 //Get Note
-router.get('/notes', renderNotes);
+router.get('/notes', isAuthenticated, renderNotes);
 
 //Edit Notes
-router.get('/notes/edit/:id', renderEditForm)
+router.get('/notes/edit/:id', isAuthenticated, renderEditForm)
 
-router.put('/notes/edit/:id', updateNote)
+router.put('/notes/edit/:id', isAuthenticated, updateNote)
 
 //Delete Note
-router.delete('/notes/delete/:id', deleteNote)
+router.delete('/notes/delete/:id', isAuthenticated, deleteNote)
 
 
 module.exports = router;
